4f56a6355cd14021b605358c340249437e3fc0be
Miles Johnson <mgj2@pdx.edu>
2023-04-19 12:54:54 -0700

    Initial REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 // backend web framework
 const express = require('express')
+const colors = require('colors')
 // Allows us to have a .env file with variables in it
 const dotenv = require('dotenv').config()
 // Port we want our server to run on
 // process.env.PORT allows us to access the .env variable in the root
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT
+
+connectDB()
 
 //initializes express
 const app = express()
